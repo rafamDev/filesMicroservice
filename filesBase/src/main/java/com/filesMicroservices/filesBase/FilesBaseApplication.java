21aca32c4962c1074e207623c3e6c5f386fb4310
rafamDev <76625223+rafamDev@users.noreply.github.com>
2023-01-29 00:11:55 +0100

    repoCreated

---
+++ filesBase/src/main/java/com/filesMicroservices/filesBase/FilesBaseApplication.java
@@ -0,0 +1,13 @@
+package com.filesMicroservices.filesBase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilesBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilesBaseApplication.class, args);
+	}
+
+}
---
+++ filesBase/src/test/java/com/filesMicroservices/filesBase/FilesBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filesMicroservices.filesBase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilesBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
